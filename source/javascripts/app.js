890871cb37432abaf651c8c0ed84d8b604576076
Guillermo Tirelli <g@tirel.li>
2015-05-25 20:25:16 +0200

    initial commit

---
+++ source/javascripts/app.js
@@ -0,0 +1,6 @@
+//= require jquery/dist/jquery.min.js
+//= require foundation/js/foundation.min.js
+
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
